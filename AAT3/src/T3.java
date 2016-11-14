364a5a75f7b127f243e78830a704ef8053dfa8dc
Max Zorzetti <Max Zorzetti>
2016-11-14 21:48:42 -0200

    Adicionado comentarios

--- AAT3/src/T3.java
+++ AAT3/src/T3.java
@@ -40,28 +40,33 @@ public class T3 {
 		return null;
 	}
 	public static String achaSequencia(ArrayList<Domino> dominos, int anterior){
-		if(dominos.size() == 0) return "";
+		if(dominos.size() == 0) return ""; // Encaixou todos os dominos
 		int proximo;
 		
 		for(int i = 0; i < dominos.size(); i++){
-			Domino d = dominos.remove(i);
+			Domino d = dominos.remove(i); // Tira um domino do "saco"
 			
-			if(d.back == anterior){
-				anterior = d.back;
-				proximo = d.front;
-			} else if(d.front == anterior){
-				anterior = d.front;
-				proximo = d.back;
+			if(d.back == anterior){				// "Encaixa" o domino, de frente ou de tras
+				anterior = d.back;				//
+				proximo = d.front;				// 
+			} else if(d.front == anterior){		// 
+				anterior = d.front;				//
+				proximo = d.back;				//
 			} else {
-				dominos.add(i, d);
-				continue;	
+				dominos.add(i, d);				// Se nao encaixou, poe de volta no
+				continue;						// saco e tenta outro
 			}
 			
-			String result = achaSequencia(dominos, proximo);
-			if(result != null) return anterior + " " + proximo + " " + result;			
+			String result = achaSequencia(dominos, proximo); // Com o domino encaixado, tenta encaixar outros
+			
+			if(result != null) return anterior + " " + proximo + " " + result;	// Retorna a sequencia ate entao, se ela foi encontrada
+																				// (so entra aqui se encaixou todos)
+			
+			dominos.add(i, d);	// Se chegou aqui, nao conseguiu encontrar uma sequencia com o domino d,
+								// entao poe ele de volta no saco
 		}
 		
-		return null;
+		return null; // Volta null se passou por todos os dominos e nao conseguiu encaixar nenhum
 	}
 	
 }
